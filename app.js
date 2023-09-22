cd6914209ed41b8914ce37c690af1d8679e1a704
Tomisin <tomisinjoseph3@gmail.com>
2023-09-22 20:58:29 +0100

    update counter

--- app.js
+++ app.js
@@ -5,35 +5,35 @@ let shop = document.getElementById('all-products');
 // map each item to the shop item format
 
 let shopData = [{
-    "id": 1,
+    "id": "1A",
         productName: "top",
         productPrice: 12.99,
         productDescription: "this is a nice top",
         productImage: "image1"
 },
 {
-    "id": 2,
+    "id": "2B",
         productName: "bottom",
         productPrice: 13.99,
         productDescription: "this is a nice bottoms",
         productImage: "image2"
 },
 {
-    "id": 3,
+    "id": "3C",
         productName: "jacket",
         productPrice: 14.99,
         productDescription: "this is a nice jacket",
         productImage: "image3"
 },
 {
-    "id": 4,
+    "id": "4D",
         productName: "hat",
         productPrice: 15.99,
         productDescription: "this is a nice hat",
         productImage: "image4"
 },
 {
-    "id": 5,
+    "id": "5E",
         productName: "shoes",
         productPrice: 16.99,
         productDescription: "this is a nice shoes",
@@ -51,15 +51,11 @@ let generateShop = () => {
         <p><a href="product.html">${productName}</a></p>
         <p>${productDescription}</p>
         <p>${productPrice}</p>
-        <div class="counter-buttons">
-
-
-
-
-            <i onclick="decrement(${id})" class="minus">-</i>
-            <div id=${id} class="quantity">0</div>
-            <i onclick="increment(${id})" class="plus">+</i>
-        </div>
+            <div class="counter-buttons-${id}">
+                <button class="buttonClass increase">+</button>
+                <span id="quantity">0</span>
+                <button class="buttonClass decrease">-</button>
+            </div>
         </div>
         `
     }).join("")
@@ -67,20 +63,48 @@ let generateShop = () => {
 
 generateShop()
 
+const quantity = document.querySelectorAll('#quantity')
+const increase = document.querySelectorAll('.increase')
+// const decrease = document.querySelector('.decrease')
+
+let count = 0
+
+function increaseCount() {
+    count += 1;
+    console.log(quantity);
+
+    quantity.forEach(amount => {
+        amount.innerHTML = count;
+        console.log(amount);
+    });
+}
+
+increase.forEach( countingclick => {
+    countingclick.addEventListener('click', increaseCount);
+});
+
+// function decreaseCount() {
+//     if (count > 0) {
+//         count -= 1;
+//         quantity.innerHTML = count;
+//     } else {
+//         return
+//     }
+// }
+
+// decrease.addEventListener('click', decreaseCount);
 
 
 
+// let increment = (id) => {
+// };
 
+// let decrement = (id) => {
+//     console.log(id);
+// };
 
-let increment = (id) => {
+// let update = () => {};
 
-};
-
-
-let decrement = (id) => {
-    console.log(id);
-};
-
-let update = () => {};
-
-
+            // <i onclick="decrement(${id})" class="minus">-</i>
+            // <div id=${id} class="quantity">0</div>
+            // <i onclick="increment(${id})" class="plus">+</i>
